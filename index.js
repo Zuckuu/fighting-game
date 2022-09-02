94384d8923895f0030a4f6e7ab1aa66b68533641
Zuckuu <entbit12@gmail.com>
2022-09-01 23:19:52 -0500

    adding characters

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 const canvas = document.querySelector('canvas'); // getting the canvas from html
 const c = canvas.getContext('2d'); // 2d because this is a 2d game
 
+
+//setting the position
+class Sprite {
+  constructor(position){
+    this.position = position;
+  }
+
+  draw() {
+    c.fillStyle = 'red'
+    c.fillRect(this.position.x, this.position.y, 10, 30)
+  }
+}
+
+const player = new Sprite({
+  x : 0,
+  y : 0,
+})
+
+player.draw();
+
+console.log(player);
